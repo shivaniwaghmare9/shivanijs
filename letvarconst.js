e78d879ce0386f4b40fb7b94efca0917a95f642f
shivani <shivaniw69@gmail.com>
2025-01-07 14:59:22 +0530

    js

--- letvarconst.js
+++ letvarconst.js
@@ -1,12 +1,69 @@
 
 
-//redeclaration
-//var supported   let &const not supported
+
 
 
 function go(){
-    var a=25;
-    vara=28;
-    console.log(a);
+  //===============Redeclaration============
+//{var supported   let &const not supported}
+   // var a=25;
+   // var a=28; ///supported
+   // console.log(a);
+
+
+//function go(){
+  // let a=25;
+    ///let a=28;  //not supported
+    //console.log(a);
+//}
+//go();
+//function go(){
+  //  const a=25;   //not supported
+   // const a=28;
+  //  console.log(a);
+//}
+//go();
+//================Reassigment=================
+//{var & let  supported   const not supported}
+
+  //var a=9;
+   // a =10;   //SUPPORTED
+   // console.log(a);
+
+
+   // let a=9;
+   // a =10;   //SUPPORTED
+   // console.log(a);
+
+
+   // const a=9;
+  //  a =10;   // not SUPPORTED
+    //console.log(a);
+
+
+    ///=====================SCOPE=====================
+     //var a=45;
+     //console.log(a)
+    // {          
+     // var a=47;       ///output 5,7
+     // console.log(a)
+    // }
+
+
+    // var a=45;
+    
+    // {          
+    //  var a=47;       ///output 7
+     //console.log(a)
+     //}
+
+     //===================HOISTING==============
+     //{supported in var == not-supported in let & const}
+
+     console.log(a);
+     var a=6;
+     var a=7;
+   
+     
 }
 go();
