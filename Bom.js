be5836dd3c1b8fd6e476d8eaf4e514b2bcc0a7ce
shivani <shivaniw69@gmail.com>
2025-01-09 14:27:14 +0530

    homework

--- Bom.js
+++ Bom.js
@@ -1,4 +1,4 @@
-function shiavni(){
+//function shiavni(){
 
        //window.alert("this is an alert message")//window na likhe to bhii chalega
   //let abc=window.confirm("Are u oky")
@@ -28,15 +28,171 @@ function shiavni(){
  //let abc=prompt("enter number")
  //if(abc%2==0){
     //alert("even number")
- //}
+ //}    
  ///else{
    // alert("odd number")
  //}
- let a1=prompt("enter  first number")
- let a2=prompt("enter  second number")
- let sum=parseInt(a1)+parseInt(a2)
- alert("sum   "+sum)
+ //========parseint========
+ //let a1=prompt("enter  first number")
+ //let a2=prompt("enter  second number")
+ //let add=parseInt(a1)+parseInt(a2)
+ //alert("add   "+add)
+//}
+//shiavni() 
+
+//==========day2=============
+//function ram(){
+  //QUESTION 1========
+
+  // let a=prompt("enter the age")
+  // if(a>=18){
+  //  alert("u can vote")
+   //}
+   
+  // else{
+  //  alert("no vote")
+  // }
+
+
+  //=======ternory operator========
+  //condition ?" on true ":"on false";
+
+   // let a=prompt("enter your age")
+   //  a>=18? alert("u can vote") :alert("u cant vote")
+  
+   //=====QUESTION 2========
+ 
+
+   // let abc=confirm("are u hpy with our website")
+   
+    //if(abc==true){
+    //  alert("tnq")
+   // }
+    //else{
+    // alert("sry")
+
+  // }
+  //========QUESTION 3========(FOR LOOP)
+
+ // for(let i=1; i<=10; ++i)
+  //console.log(i)
+
+//}
+//ram();
+//====================(HOME WORK)===============================
+
+function shiv(){
+                //====[QUESTION 1]============
+
+  //=========AGE(GREATER THAN OR EQUAL)================
+//  let a=prompt("enter your age")
+//  a>=18? alert("u are senior ") :alert("u are junior")
+
+
+
+               //====[QUESTION 2]============
+
+
+  //===========(COMPLETED COURSE)====================
+//  let b=confirm("is your course complete?")
+//  if(b==true)
+ // {
+//    alert("comleted")
+//  }
+//  else{
+//    alert("not completed")
+ // }
+
+
+             //====[QUESTION 3]============
+
+ //================(EXAM SCORE)====================
+//let c=prompt("enter your score")
+//if(c>=33)
+//{
+ // alert("passed")
+//}
+//else{
+ // alert("failed")
+//}
+
+
+             //====[QUESTION 4]============
+
+
+//=================(EVEN OR ODD)==================
+ //let d=prompt("enter number")
+ //if(d%2==0){
+ // alert("even number")
+// }    
+ //else{
+ //   alert("odd number")
+ //}
+
+                      //====[QUESTION 5]============
+
+
+ //==================(DELETE MESSAGE)===============
+ //let e=confirm("r u sure to delete this msg")
+// if(e==true)
+// {
+//  alert("item delete")
+// }
+// else{
+//  alert("cancelled")
+ //}
+                   
+                        //====[QUESTION 6]============
+
+
+ //====================(+TIVE OR -TIVE)=================
+ //let f=prompt("enter a number")
+// if(f>0){
+ // alert("+tive")
+ //}
+ //else if(f<0){
+ // alert("-tive")
+ //}
+ //else{
+ // alert("zero")
+ //}
+                       //====[QUESTION 7]============
+
+
+ //================( 1-10 NUMBER WHILE LOOP)==================
+ //let i=1;
+ //while (i<=10)
+ //{
+ // console.log(i);
+//  i++
+// }
+                   //====[QUESTION 8]============
+
+
+//===================(WEEK DAY )===================================
+//let day=parseInt(prompt("enter a day of the week (1-7)"))
+//switch(day){
+  //case 1:alert("monday")
+  //  break;
+  //  case 2:alert("tuesday")
+  //  break;
+  //  case 3: alert("wednesday")
+  //  break;
+  //  case 4: alert("thursday")
+  //  break;
+  //  case 5: alert("friday")
+  //  break;
+  //  case 6:alert("saturday")
+  //  break;
+  //  case 7: alert("sunday")
+  //  break;
+  //  default:alert("invalid input")
+//}
+
+                        //====[QUESTION 9]============
+
+//==========================()
 
 
 }
-shiavni()    
+shiv();
