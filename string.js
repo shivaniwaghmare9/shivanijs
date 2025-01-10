c65b84d91c2f89a146577f818d6f2e5945bd667c
shivani <shivaniw69@gmail.com>
2025-01-10 18:30:41 +0530

    document

--- string.js
+++ string.js
@@ -1,21 +1,48 @@
 
 
 
-function shivani(){
-    alert("Hello")
-}
+//function shivani(){
+//    alert("Hello")
+//}
 //shivani();
 
-function ram(){
+//function ram(){
 
-    alert("ram")
-}
+   // alert("ram")
+//}
 //ram();
-function boy(){
+//function boy(){
 
-    alert("tahir")
+//   alert("tahir")
+//}
+//function hey(){
+ //   alert("interation with the browser window")///browser window
+//}
+//hey();
+//====================================================day 4========================
+function shiv(){
+   //browser window
+   //console.log()
+//  document.write("<h1>hello</h1>")  mtlb [html page]
+   //  let  text="this is string"
+    // document.write(text.length)  to  check length of string
+  //let text="javascriptiseasy";
+   // document.write(text.slice(4,13)) ///slice me indexing 0se start hoti or length se 1-indexing phle end homjati h
+
+ // let text1="shiv"
+ // let text2="ani"
+
+    //document.write(text1.concat(text2))   //       //concat
+   // let text1="shiv"
+  //let text2="ani"
+  //  document.write(text1.toLowerCase(text2))
+
+    let text1="shiv"
+    let text2="ani"
+      document.write(text1.toUpperCase(text2))
+  
+
 }
-function hey(){
-    alert("interation with the browser window")
-}
-hey();
+//shiv();
+
+//====================================================
