a109f8924b9db071c5da7ba1758bdcec03eb47f6
shivani <shivaniw69@gmail.com>
2025-01-20 21:04:20 +0530

    new

--- DOM.js
+++ DOM.js
@@ -6,7 +6,16 @@
     //para.innerHTML="HELLO I AM DEVELOPER"
 
 
-//}
+//}  
+  function textchange(){
+   let change= document.querySelector("#box");
+   change.style.backgroundImage="url('img 1.jpg')";         //backgroung img
+
+  }
+  function new1(){
+   let change= document.querySelector("#box");
+   change.style.backgroundImage="none";
+  }
    /*function textchange(){
     let change= document.querySelector("#box")
     change.innerHTML="<h1>Hello Cybrom</h1>"  
@@ -25,14 +34,39 @@
    function ram(){
     document.body.style.backgroundColor="white"
    }*/
-  function ram() {
-   let change=  document.querySelector("#new")
-   change.addEventListener('click',()=>{
+ /* function ram() {
+   let change= document.querySelector("#new")
+   if(change.style.display==='none'){
+      change.style.display='block';
+   }
+   else{
+      change.style.display='none';
+   }*/
+
+  /* let change1=0;
+   function ram() {
+      if(change1==1){
+         change.style.display="none";
+         change.style.display="block";
+         change =0;
+
+      }
+      else{
+         change.style.display ="none";
+         change1=1;
+      }*/
+
+   /*let change=  document.querySelector("#new")
+   let change1= document.querySelector("#btn1"))
+  change1.addEventListener('click',()=>{)
       if(change.style.display==='none'){
          change.style.display='block';
       }
       else{
          change.style.display='none';
       }
-   })
-  }
+})*/
+   
+
+  // }
+//  ram();
