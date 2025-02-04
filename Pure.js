c93c9c5f6ce06243f2f26139d1aa8c1eb5917755
shivani <shivaniw69@gmail.com>
2025-02-05 00:10:13 +0530

    purejs

---
+++ Pure.js
@@ -0,0 +1,23 @@
+
+
+// let clickme=document.querySelector("#btn")
+// let clickm=document.querySelector("#btn1")
+// clickme.addEventListener("dblclick",()=>{
+//     clickm.style.backgroundColor="red"
+
+    
+    
+// })
+// let clc=document.querySelector("#btn1")
+// let clickm=document.querySelector("#btn2")
+// clc.addEventListener("click",()=>{
+//     clickm.style.backgroundColor="orange"
+// })
+
+let inpbox=document.querySelector("#inpbox")
+// // inpbox.addEventListener("keydown",(e)=>{
+// //     console.log(e.key);
+// })
+inpbox.addEventListener("input",()=>{
+    console.log(inpbox.Value)
+})
