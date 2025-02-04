6e11499b55ae67150e13464f10a40cf877674650
shivani <shivaniw69@gmail.com>
2025-02-05 00:51:09 +0530

    hwork

--- Pure.js
+++ Pure.js
@@ -1,23 +1,34 @@
 
 
 // let clickme=document.querySelector("#btn")
+
+// clickme.addEventListener("click",()=>{  ///ek bar click larne prr
+//     alert("Hello")
+// })
+
+
+// let clc=document.querySelector("#btn2")
 // let clickm=document.querySelector("#btn1")
-// clickme.addEventListener("dblclick",()=>{
-//     clickm.style.backgroundColor="red"
-
-    
-    
-// })
-// let clc=document.querySelector("#btn1")
-// let clickm=document.querySelector("#btn2")
-// clc.addEventListener("click",()=>{
-//     clickm.style.backgroundColor="orange"
+// clc.addEventListener("dblclick",()=>{  ///dblclick karne prr 
+//     clickm.style.backgroundColor="blue"
 // })
 
-let inpbox=document.querySelector("#inpbox")
-// // inpbox.addEventListener("keydown",(e)=>{
-// //     console.log(e.key);
+
+
+// let inpbox=document.querySelector("#inpbox")
+//  inpbox.addEventListener("keydown",(e)=>{
+//      console.log(e.key);
 // })
-inpbox.addEventListener("input",()=>{
-    console.log(inpbox.Value)
+
+
+// let inpbox1=document.querySelector("#inpbox")
+// inpbox1.addEventListener("input",()=>{
+//     console.log(inpbox1.Value)
+// })
+
+let show=document.querySelector("#input")
+let dispaly=document.querySelector("#button")
+show.addEventListener("keydown",(e)=>{
+    dispaly.innerHTML=(e.key)
+
 })
