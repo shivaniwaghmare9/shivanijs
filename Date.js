6263d7d673be6c29557db0f8fe4744d4312eb202
shivani <shivaniw69@gmail.com>
2025-02-13 17:32:24 +0530

    alarm

--- Date.js
+++ Date.js
@@ -1,29 +1,61 @@
 
 
 
-//console.log(new Date());
+// //console.log(new Date());
 
-let time=new  Date()
- console.log(time.toLocaleString());  //readable
+// let time=new  Date()
+//  console.log(time.toLocaleString());  //readable
 
-console.log(time.getFullYear());  //year 2025
-console.log(time.getMonth());
-console.log(time.getDay());
-console.log(time.getMinutes());
-console.log(time.getSeconds());
-console.log(time.getMilliseconds());
-console.log(time.getHours());
+// console.log(time.getFullYear());  //year 2025
+// console.log(time.getMonth());
+// console.log(time.getDay());
+// console.log(time.getMinutes());
+// console.log(time.getSeconds());
+// console.log(time.getMilliseconds());
+// console.log(time.getHours());
 
 
  
-let a=["sunday","monday","tuesday","wednusday","thursday","friday","saturday"]
-console.log(a[time.getDay()])
+// let a=["sunday","monday","tuesday","wednusday","thursday","friday","saturday"]
+// console.log(a[time.getDay()])
 
 
-let shiv=()=>{
+// let shiv=()=>{
 
- let change=  document.querySelector("#btn")
- let a=["sunday","monday","tuesday","wednusday","thursday","friday","saturday"]
-change.innerHTML=a[time.getDay()]
+//  let change=  document.querySelector("#btn")
+//  let a=["sunday","monday","tuesday","wednusday","thursday","friday","saturday"]
+// change.innerHTML=a[time.getDay()]
 
+// }
+
+
+
+// let ram=()=>{
+//      let show=document.querySelector("#btn1")
+//      let display=document.querySelector("#inp").value;
+//      let current=new Date();
+//      let age=current.getFullYear() - display;
+//      show.innerHTML=`you are ${age} years old`;
+
+// }
+
+// let shiv=()=>{
+//      let show=document.querySelector("#btn2")
+//      let display=new Date();
+//      show.innerHTML=display.toLocaleTimeString()
+// }
+   //setInterval(time,1000)  OR
+
+// setInterval(()=>{
+//       let show=document.querySelector("#btn2")
+//      let display=new Date();
+//      show.innerHTML=display.toLocaleTimeString()
+// },1000)
+
+let time=()=>{
+     let ring=document.querySelector("#alm")
+     let display=document.querySelector("#inp").value
+     let current=new Date();
+     let time=current.toLocaleTimeString()
+     ring.innerHTML=`alarm is ringing ${display}`;
 }
