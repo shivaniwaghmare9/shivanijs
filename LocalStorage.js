71a2b0f7e15db160ee56c17cf8976fe9c2eca979
shivani <shivaniw69@gmail.com>
2025-02-17 17:33:09 +0530

    new

---
+++ LocalStorage.js
@@ -0,0 +1,24 @@
+
+
+let senddata=()=>{
+
+    let naame=document.querySelector("#inp").value
+    localStorage.setItem("Name",naame)
+
+    localStorage.setItem("Name","shivani")
+    localStorage.setItem("Age","23")
+    localStorage.setItem("Course","CS")
+    localStorage.setItem("Rollno","236")
+}
+let show=document.querySelector("#display")
+show.innerHTML=localStorage.getItem("Name")
+
+
+let out=()=>{
+
+localStorage.removeItem("Name")
+//localStorage.clear()      //all data clear
+location.reload()
+ 
+
+}
--- alarm.js
+++ alarm.js
@@ -1,24 +1,29 @@
 
-
-let set=()=>{
+let stp;
+let sett=()=>{
     let inputtime=document.querySelector("#curtime").value   //4:53
     let dispaly=document.querySelector("#show")
-    let ring=document.querySelector("#music")
-   let stp= setInterval(()=>{
+    let ringg=document.querySelector("#music")
+    stp=setInterval(()=>{
         let time=new Date()
-        let currenttime=`${time.getHours().toString().padStart(2,0)}:${time.getMinutes().toString().padStart(2,0)}`     //4:
+        let currenttime=`${time.getHours().toString().padStart(2,"0")}:${time.getMinutes().toString().padStart(2,"0")}`     //4:
         if(currenttime==inputtime){
             dispaly.innerHTML="Alarm is ringing...."
-             ring.play()
+            dispaly.style.color="darkgoldenrod"
+             ringg.play()
         }
         else{
             dispaly.innerHTML="Alarm is set.."
         }
         
+      
     },1000)
-    setTimeout(()=>{
-        clearInterval(stp)
-        ring.pause()
-        },5000)
 
 }
+// let stop=()=>{
+//     setTimeout(()=>{
+//              clearInterval(stp)
+//             ringg.pause()
+//              })
+// }
+
